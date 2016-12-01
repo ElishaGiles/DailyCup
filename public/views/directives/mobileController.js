7a7a6100b91e07678dd3a493c6cc90d9a907f29c
Elisha Giles <elisha.g.giles@gmail.com>
2016-12-01 12:04:04 -0700

    redid the contact page to fit my laptop

--- public/views/directives/mobileController.js
+++
@@ -1,7 +0,0 @@
-angular.module('tiara').controller('mobileController', function($scope) {
-
-  $scope.toggleMenu = function() {
-    $scope.menuOpen = !$scope.menuOpen;
-  }
-
-})
--- public/views/directives/topMenuDirective.js
+++
@@ -1,8 +0,0 @@
-angular.module('tiara').directive('topMenuDirective', function() {
-
-  return {
-    restrict: 'EA',
-    templateUrl: "./views/directives/topMenuDirective.html"
-  }
-
-})
