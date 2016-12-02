f87c000d67c65dde8a2ee7253ef2d330fc1580f3
Elisha Giles <elisha.g.giles@gmail.com>
2016-12-01 17:39:30 -0700

    completed styling for enlightenment page

--- public/app.js
+++ public/app.js
@@ -3,37 +3,31 @@ angular.module('tiara', ['ui.router', 'ngAnimate']).config(function($stateProvid
   $stateProvider
     .state('home', {
       url: '/',
-      templateUrl: "./views/home.html"
+      templateUrl: "../views/home.html"
     })
     .state('meet-tiara', {
       url: '/meet-tiara',
-      templateUrl: "./views/meet.html",
-      controller: "mobileController"
+      templateUrl: "../views/meet.html"
     })
     .state('menu', {
       url: '/meet-tiara',
-      templateUrl: "./views/mobile.html",
-      controller: "mobileController"
-
+      templateUrl: "../views/mobile.html"
     })
     .state('fitness', {
       url: "/fitness",
-      templateUrl: "./views/fitness.html",
-      controller: "mobileController"
+      templateUrl: "../views/fitness.html"
     })
     .state('enlightenment', {
       url: "/enlightenment",
-      templateUrl: "./views/enlightenment.html",
-      controller: "mobileController"
+      templateUrl: "../views/enlightenment.html"
     })
     .state('sustainability', {
       url: "/sustainability",
-      templateUrl: "./views/sustainability.html",
-      controller: "mobileController"
+      templateUrl: "../views/sustainability.html"
     })
     .state('contact', {
       url: "/contact",
-      templateUrl: "./views/contact.html",
+      templateUrl: "../views/contact.html",
       controller: 'contact-ctrl'
     })
 
