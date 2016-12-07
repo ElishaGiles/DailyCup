47d8a4d5916e58757bd0363e986be4c52e3d42d2
Elisha Giles <elisha.g.giles@gmail.com>
2016-12-07 16:46:35 -0700

    weather API's working

--- public/views/services/weekWeatherSvc.js
+++ public/views/services/weekWeatherSvc.js
@@ -9,15 +9,12 @@ angular.module('tiara').service('weekWeatherSvc', function($http) {
 
               var weekObject = {};
               if (response.status === 200) {
-                  weekObject.temp1 = response.data.main[0].temp.day;
-                  weekObject.humidity1 = response.data.main[0].humidity;
-                  weekObject.windSpeed1 = response.data.main[0].speed;
-                  weekObject.temp2 = response.data.main[1].temp.day;
-                  weekObject.humidity2 = response.data.main[1].humidity;
-                  weekObject.windSpeed2 = response.data.main[1].speed;
-                  weekObject.temp3 = response.data.main[2].temp.day;
-                  weekObject.humidity3 = response.data.main[2].humidity;
-                  weekObject.windSpeed3 = response.data.main[2].speed;
+                  weekObject.temp2 = response.data.list[1].temp.day;
+                  weekObject.humidity2 = response.data.list[1].humidity;
+                  weekObject.windSpeed2 = response.data.list[1].speed;
+                  weekObject.temp3 = response.data.list[2].temp.day;
+                  weekObject.humidity3 = response.data.list[2].humidity;
+                  weekObject.windSpeed3 = response.data.list[2].speed;
                   return weekObject;
               }
               return "It's broken, sorry!";
