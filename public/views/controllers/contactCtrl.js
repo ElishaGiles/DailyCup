f8462e66d5cd581280eff4d40099ba6bbee7d6c0
Elisha Giles <elisha.g.giles@gmail.com>
2016-12-12 09:27:28 -0700

    making changes to contact page

--- public/views/controllers/contactCtrl.js
+++ public/views/controllers/contactCtrl.js
@@ -1,9 +1,9 @@
 angular.module('tiara').controller('contact-ctrl', function($scope, contactService) {
 
   $scope.sendForm = function(form) {
-    contactService.sendForm(form).then(function(response) {
-      if(response.data === 'err') {
-        console.log((err));
+    contactService.sendForm(form).then(function(response, err) {
+      if(response.data === err) {
+        console.log(('err'));
         alert('There was an error');
       }
       else {
